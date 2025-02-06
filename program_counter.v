75a7c81a39ea841f43b7f2114a51b2ace61d964f
Syed Muhammad Aun Hasan Naqavi <122987113+MuhammadAun-gif@users.noreply.github.com>
2025-02-06 22:38:10 +0500

    Create program_counter.v

---
+++ program_counter.v
@@ -0,0 +1,13 @@
+module program_counter(input clk, rst,
+                       input [31:0] PC_Next
+                       output reg [31:0] PC);
+  
+    always @(posedge clk)
+    begin
+        if(~rst)
+            PC <= {32{1'b0}};
+        else
+            PC <= PC_Next;
+    end
+  
+endmodule
