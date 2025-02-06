7673f4e276d3141d9de3f5f798790f47bd5f0091
Syed Muhammad Aun Hasan Naqavi <122987113+MuhammadAun-gif@users.noreply.github.com>
2025-02-06 22:22:35 +0500

    Create alu.v

---
+++ alu.v
@@ -0,0 +1,24 @@
+module alu(input [31:0] A,B,
+            input [2:0] ALUControl,
+            input OverFlow,Carry,Zero,Negative
+           output [31:0] Result);
+    
+    wire Cout;
+    wire [31:0] sum;
+
+    assign {Cout,sum} = (ALUControl[0] == 1'b0) ? A + B :
+                                          (A + ((~B)+1)) ;
+    assign Result = (ALUControl == 3'b000) ? sum :
+                    (ALUControl == 3'b001) ? sum :
+                    (ALUControl == 3'b010) ? A & B :
+                    (ALUControl == 3'b011) ? A | B :
+                    (ALUControl == 3'b101) ? {{31{1'b0}},(sum[31])} : {32{1'b0}};
+    
+    assign OverFlow = ((sum[31] ^ A[31]) & 
+                      (~(ALUControl[0] ^ B[31] ^ A[31])) &
+                      (~ALUControl[1]));
+    assign Carry = ((~ALUControl[1]) & Cout);
+    assign Zero = &(~Result);
+    assign Negative = Result[31];
+
+endmodule
