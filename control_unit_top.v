a1d75dbb45f943ecb58138f652bc9c46929121f7
Syed Muhammad Aun Hasan Naqavi <122987113+MuhammadAun-gif@users.noreply.github.com>
2025-02-06 22:28:58 +0500

    Create control_unit_top.v

---
+++ control_unit_top.v
@@ -0,0 +1,33 @@
+`include "alu_decoder.v"
+`include "main_decoder.v"
+
+module control_unit_top(input [6:0] Op, funct7,
+                        input [2:0] funct3, 
+                        output RegWrite, ALUSrc, MemWrite, ResultSrc, Branch, PCSrc,
+                        output [1:0] ImmSrc,
+                        output [1:0] ALUControl);
+  
+    wire [1:0]ALUOp;
+
+    main_decoder Main_Decoder(
+                .Op(Op),
+                .RegWrite(RegWrite),
+                .ImmSrc(ImmSrc),
+                .MemWrite(MemWrite),
+                .ResultSrc(ResultSrc),
+                .PCSrc(PCSrc),
+                .Branch(Branch),
+                .ALUSrc(ALUSrc),
+                .ALUOp(ALUOp)
+    );
+
+    alu_decoder ALU_Decoder(
+                            .ALUOp(ALUOp),
+                            .funct3(funct3),
+                            .funct7(funct7),
+                            .op(Op),
+                            .ALUControl(ALUControl)
+    );
+
+
+endmodule
