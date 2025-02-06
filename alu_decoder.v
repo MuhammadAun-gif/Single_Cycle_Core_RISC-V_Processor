85e0c7f98b9eae28446f333bce834fe85fc16cd2
Syed Muhammad Aun Hasan Naqavi <122987113+MuhammadAun-gif@users.noreply.github.com>
2025-02-06 23:30:27 +0500

    Update alu_decoder.v

--- alu_decoder.v
+++ alu_decoder.v
@@ -1,9 +1,7 @@
-module ALU_Decoder(ALUOp,funct3,funct7,op,ALUControl);
-
-    input [1:0]ALUOp;
-    input [2:0]funct3;
-    input [6:0]funct7,op;
-    output [2:0]ALUControl;
+module ALU_Decoder(input [1:0] ALUOp,
+                    input [2:0] funct3,
+                    input [6:0] funct7, op,
+                    output [2:0] ALUControl);
 
     assign ALUControl = (ALUOp == 2'b00) ? 3'b000 :
                         (ALUOp == 2'b01) ? 3'b001 :
